99f7dffa345060eefb27aa033c1f54ad59b91c30
GaboramaTrabajosDuoc <gabr.ramirezm@duocuc.cl>
2025-03-29 16:37:53 -0300

    29-03-2025 en caso de quedarme sin bateria

--- src/templates/DailyTasks.jsx
+++ src/templates/DailyTasks.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // src/templates/DailyTasks.jsx
 import React, { useState } from "react";
 import "../css/DailyTasks.css";
@@ -186,71 +185,4 @@ return (
     </div>
   );
 };
-=======
-import React, { useState } from "react";
-import '../css/DailyTasks.css';
-import Modal from "react-modal";
-import List from "./newAddingTasks";
-
-// Function to open modal
-function popModal(setModalIsOpen) {
-    setModalIsOpen(true);
-}
-
-// Function to close modal
-function closeModal(setModalIsOpen) {
-    setModalIsOpen(false);
-}
-
-// Component definition
-const DailyTasks = () => {
-    // Initialize modal state
-    const [modalIsOpen, setModalIsOpen] = useState(false);
-
-    return (
-        <div className="body">
-            <header>
-                <div className="logo">4'🕓</div>
-            </header>
-
-            <footer>
-                <nav>
-                    <ul className="footer-nav-links">
-                        <a className="cta" href="#"><button>Daily Tasks</button></a>
-                        <a className="cta" href="#"><button>Power Lists</button></a>
-                        <li>
-                            <a className="add-button" onClick={() => popModal(setModalIsOpen)}>
-                                <button>
-                                    <span>+
-                                        <br/>
-                                        NEW TASK
-                                    </span>
-                                </button>
-                            </a>
-                        </li>
-                        <a className="cta" href="#"><button>Comunity</button></a>
-                        <a className="cta" href="#"><button>Settings</button></a>
-                    </ul>
-                </nav>
-            </footer>
-
-            {/* Modal Code*/}
-            <Modal isOpen={modalIsOpen} onRequestClose={() => closeModal(setModalIsOpen)} className="add-task-modal">
-                <List/>
-                <button onClick={() => closeModal(setModalIsOpen)}>Close</button>
-            </Modal>
-        </div>
-    );
-}
-
-// Component definition for NewaddingTasks
-const NewaddingTasks = () => {
-    return (
-        <div>
-            {/* Add your content here */}
-        </div>
-    );
-}
->>>>>>> 1399c3b5f0fc0c14b2ffdc0549544c77c19e2493
-
 export default DailyTasks;
